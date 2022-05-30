1675ba327c3366ac58071d03beb76e2d6bb65af4
Gabriel Luong <gabriel.luong@gmail.com>
2022-05-29 22:13:26 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,106 @@
+const github = require("@actions/github");
+const core = require('@actions/core');
+
+/**
+ * Returns a list of issue numbers from parsing the commits in the pull request.
+ */
+function getIssues(commits) {
+  const issueRegExp = new RegExp(core.getInput("commit-regexp"), "g");
+  const issues = [];
+
+  for (const { commit } of commits) {
+    if (commit.message.startsWith("Revert")) {
+      continue;
+    }
+
+    const result = issueRegExp[Symbol.matchAll](commit.message);
+    const issueNumbers = Array.from(result, x => x[1]);
+
+    for (const issueNumber of issueNumbers) {
+      if (!issues.includes(issueNumber)) {
+        issues.push(issueNumber);
+      }
+    }
+  }
+
+  return issues;
+}
+
+async function run() {
+  try {
+    const token = core.getInput("github-token");
+    const octokit = new github.getOctokit(token);
+    const payload = github.context.payload;
+    const repo = payload.repository.name;
+    const owner = payload.repository.owner.login;
+    const pullRequestNumber = payload.number;
+
+    if (pullRequestNumber === undefined) {
+      core.warning("No pull request number in payload.");
+      return;
+    }
+
+    const commits = await octokit.rest.pulls.listCommits({
+      owner,
+      repo,
+      pull_number: pullRequestNumber,
+    });
+
+    const issues = getIssues(commits.data);
+
+    if (!issues.length) {
+      core.warning("No issue numbers found in commits.");
+      return;
+    }
+
+    const pull = await octokit.rest.pulls.get({
+      owner,
+      repo,
+      pull_number: pullRequestNumber,
+    });
+
+    const checkRegExp = new RegExp(core.getInput("check-regexp"));
+    let body = pull.data.body;
+
+    if (body == null || !checkRegExp.test(body)) {
+      core.warning("Check was not found.");
+      return;
+    }
+
+    const match = checkRegExp.exec(body);
+    const isChecked = match[1].toLowerCase() == "x";
+
+    for (const issueNumber of issues) {
+      const issue = await octokit.rest.issues.get({
+        owner,
+        repo,
+        issue_number: issueNumber
+      });
+      let labels = issue.data.labels.map(label => label.name);
+      const label = core.getInput("label");
+
+      if ((isChecked && labels.includes(label)) ||
+          (!isChecked && !labels.includes(label))
+      ) {
+        continue;
+      } else if (!isChecked && labels.includes(label)) {
+        labels = labels.filter(l => l != label);
+      } else if (isChecked && !labels.includes(label)) {
+        labels.push(label);
+      }
+
+      await octokit.rest.issues.update({
+        owner,
+        repo,
+        issue_number: issueNumber,
+        labels
+      });
+
+      core.notice(`Added ${label} in #${issueNumber}.`);
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
